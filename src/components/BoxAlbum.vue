795a5b0c5a40b0afa361bfda31a14957b9f51ac5
francescolucco <francescolucco@googlemail.com>
2021-11-26 17:27:25 +0100

    creazione componente loader

--- src/components/BoxAlbum.vue
+++ src/components/BoxAlbum.vue
@@ -1,28 +1,34 @@
 <template>
-   <div class="row">
-      <Album
-         v-for="(album, index) in albums"
-         :key="index"
-         :infoAlbum = "album"
-         />
+   <div>
+      <div v-if="loaded" class="row">
+         <Album
+            v-for="(album, index) in albums"
+            :key="index"
+            :infoAlbum = "album"
+            />
+      </div>
+      <Loader v-else titleLoader="Dischi anni '70 '80 '90 '00"/>
    </div>
 </template>
 
 <script>
 
 import Album from "./Album.vue";
+import Loader from "./Loader.vue";
 import axios from "axios";
 
 
 export default {
    name: 'BoxAlbum',
    components: {
-      Album
+      Album,
+      Loader
    },
    data(){
       return{
          urlApiBoolean: 'https://flynn.boolean.careers/exercises/api/array/music',
          albums: [],
+         loaded: false
       }
    },
    methods:{
@@ -32,7 +38,6 @@ export default {
                this.albums = response.data.response;
                console.log(this.albums);
             })
-         
       }
    },
    mounted(){
