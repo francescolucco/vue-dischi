2c092ae8102eb83d68686287228cc3977c15bd9c
francescolucco <francescolucco@googlemail.com>
2021-11-26 12:36:45 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+
+@import './assets/scss/vars.scss';
+@import './assets/scss/mixin.scss';
+@import './assets/scss/generals.scss';
+@import './assets/scss/utilities.scss';
+
+</style>
+
